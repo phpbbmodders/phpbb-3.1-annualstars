58e567eca641df7088c83cfa3ae2460ef739426c
Richard McGirr <rmcgirr83@gmail.com>
2014-12-02 06:37:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+phpbb-3.1-annualstars
+=========================
+
+Extension will display stars for the number of years a user has been registered on a forum.
+
+This extension is the 3.1.x version of the 3.0.x Annual Stars Mod](https://www.phpbb.com/customise/db/mod/annual_stars_2/)
+
+## Installation
+
+### 1. clone
+Clone (or download and move) the repository into the folder ext/phpbbmodders/annualstars:
+
+```
+cd phpBB3
+git clone https://github.com/phpbbmodders/phpbb-3.1-annualstars.git ext/phpbbmodders/annualstars/
+```
+
+### 2. activate
+Go to admin panel -> tab customise -> Manage extensions -> enable Annual Stars
+
---
+++ event/listener.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+*
+* Annual Stars extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014 RMcGirr83
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace phpbbmodders\annualstars\event;
+
+/**
+* Event listener
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class listener implements EventSubscriberInterface
+{
+	/** @var \phpbb\template\template */
+	protected $template;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.viewtopic_cache_user_data'	=> 'viewtopic_cache_user_data',
+			'core.viewtopic_modify_post_row'	=> 'viewtopic_modify_post_row',
+			'core.memberlist_view_profile'		=> 'memberlist_view_profile',
+		);
+	}
+
+	/**
+	* Constructor
+	* NOTE: The parameters of this method must match in order and type with
+	* the dependencies defined in the services.yml file for this service.
+	*
+	* @param \phpbb\user	$user		User object
+	*/
+	public function __construct(\phpbb\template\template $template, \phpbb\user $user)
+	{
+		$this->template = $template;
+		$this->user = $user;
+	}
+
+	public function viewtopic_cache_user_data($event)
+	{
+		// we don't care about anonymous posters
+		if ($event['poster_id'] == ANONYMOUS)
+		{
+			return;
+		}
+		$array = $event['user_cache_data'];
+		$stars = $this->annual_stars($event['row']['user_regdate']);
+		$array['annual_stars'] = $stars;
+		$event['user_cache_data'] = $array;
+	}
+
+	public function viewtopic_modify_post_row($event)
+	{
+		$event['post_row'] = array_merge($event['post_row'], array('ANNUAL_STARS' => $event['user_poster_data']['annual_stars']));
+	}
+
+	public function memberlist_view_profile($event)
+	{
+		$stars = $this->annual_stars($event['member']['user_regdate']);
+		
+		$this->template->assign_vars(array(
+			'ANNUAL_STARS'	=> $stars,
+		));
+	}
+
+	private function annual_stars($reg_date)
+	{
+		$this->user->add_lang_ext('phpbbmodders/annualstars', 'annualstars');
+
+		$stars = '';
+		if ($reg_years = (int) ((time() - (int) $reg_date) / 31536000))
+		{
+			$reg_output = sprintf($this->user->lang['YEAR_OF_MEMBERSHIP'], $reg_years);
+			
+			if($reg_years > 1)
+			{
+				$reg_output = sprintf($this->user->lang['YEARS_OF_MEMBERSHIP'], $reg_years);
+			}
+			$stars = str_repeat('<img src="./ext/phpbbmodders/annualstars/styles/prosilver/imageset/icon_annual_star.gif" title="' . $reg_output . '" alt="' . $reg_output . '" />', $reg_years);
+		}
+		return $stars;
+	}
+}
